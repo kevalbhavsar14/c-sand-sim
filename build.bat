cf371cd9cd65bcf0b90a8f5fef30d0830694725f
Keval Bhavsar <kevalbhavsar1404@gmail.com>
2024-02-16 00:24:37 +0530

    removed -Werror flag from build scripts

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 rem This is the path for my windows installation of Raylib
 set INCLUDE_PATH="C:\raylib\raylib\src"
 set LIBRARY_PATH="C:\raylib\raylib\src"
-set CFLAGS=-O3 -Wall -Werror -Wextra
+set CFLAGS=-O3 -Wall -Wextra
 set LIBS=-lraylib -lopengl32 -lgdi32 -lwinmm
 @echo on
 gcc sandSim.c %CFLAGS% -o sandSim.exe -I %INCLUDE_PATH% -L %LIBRARY_PATH% %LIBS%
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 INCLUDE_PATH="your_raylib_path"
 LIBRARY_PATH="your_raylib_path"
-CFLAGS="-O3 -Wall -Werror -Wextra"
+CFLAGS="-O3 -Wall -Wextra"
 LIBS="-lraylib -lm" # add neccessary libs since this is not tested
 gcc sandSim.c $CFLAGS -o sandSim -I $INCLUDE_PATH -L $LIBRARY_PATH $LIBS
